95cb35cd2d4cbad12a21aded89e85908d972a60c
Flibouche <kevin.pfiffer2@gmail.com>
2024-09-18 11:39:32 +0200

    Add game constants.

--- Scenes/Scripts/Characters/Player/Player.cs
+++ Scenes/Scripts/Characters/Player/Player.cs
@@ -9,11 +9,13 @@ public partial class Player : CharacterBody3D
 
     private Vector2 direction = new();
 
+    // Called when the node enters the scene tree for the first time
     public override void _Ready()
     {
-        animPlayerNode.Play("Idle");
+        animPlayerNode.Play(GameConstants.ANIM_IDLE);
     }
 
+    // Called every frame
     public override void _PhysicsProcess(double delta)
     {
         Velocity = new(direction.X, 0, direction.Y);
@@ -25,16 +27,19 @@ public partial class Player : CharacterBody3D
     public override void _Input(InputEvent @event)
     {
         direction = Input.GetVector(
-            "MoveLeft", "MoveRight", "MoveForward", "MoveBackward"
+            GameConstants.INPUT_MOVE_LEFT, // Negative X
+            GameConstants.INPUT_MOVE_RIGHT, // Positive X
+            GameConstants.INPUT_MOVE_FORWARD, // Negative Y
+            GameConstants.INPUT_MOVE_BACKWARD // Positive Y
         );
 
         if (direction == Vector2.Zero)
         {
-            animPlayerNode.Play("Idle");
+            animPlayerNode.Play(GameConstants.ANIM_IDLE);
         }
         else
         {
-            animPlayerNode.Play("Move");
+            animPlayerNode.Play(GameConstants.ANIM_MOVE);
         }
     }
 }
