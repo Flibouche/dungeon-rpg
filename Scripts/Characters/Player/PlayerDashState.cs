d32cae2e8cdd9b00920ddf72f51b3f9f43d5c992
Flibouche <kevin.pfiffer2@gmail.com>
2024-12-03 11:40:46 +0100

    📦 NEW: Finalizing the Dash State with speed control and improved movement handling.

--- Scripts/Characters/Player/PlayerDashState.cs
+++ Scripts/Characters/Player/PlayerDashState.cs
@@ -6,6 +6,8 @@ public partial class PlayerDashState : Node
     private Player characterNode;
     [Export] private Timer dashTimerNode;
 
+    [Export] private float speed = 10;
+
     public override void _Ready()
     {
         characterNode = GetOwner<Player>();
@@ -19,12 +21,29 @@ public partial class PlayerDashState : Node
         if (what == 5001)
         {
             characterNode.animPlayerNode.Play(GameConstants.ANIM_DASH);
+            characterNode.Velocity = new(characterNode.direction.X, 0, characterNode.direction.Y);
+
+            if (characterNode.Velocity == Vector3.Zero)
+            {
+                characterNode.Velocity = characterNode.spriteNode.FlipH ?
+                    Vector3.Left :
+                    Vector3.Right;
+            }
+
+            characterNode.Velocity *= speed;
             dashTimerNode.Start();
         }
     }
 
+    public override void _PhysicsProcess(double delta)
+    {
+        characterNode.MoveAndSlide();
+        characterNode.Flip();
+    }
+
     private void HandleDashTimeout()
     {
+        characterNode.Velocity = Vector3.Zero;
         characterNode.stateMachineNode.SwitchState<PlayerIdleState>();
     }
 }
